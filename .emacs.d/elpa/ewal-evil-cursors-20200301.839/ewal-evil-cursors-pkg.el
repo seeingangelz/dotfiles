07412ff932ad2a59b4a7911e73fcd280eff59493
aeon <aeonsec@keemail.me>
2023-02-02 17:45:00 -0300

    dotfiles1

--- .emacs.d/elpa/ewal-evil-cursors-20200301.839/ewal-evil-cursors-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from ewal-evil-cursors.el  -*- no-byte-compile: t -*-
-(define-package "ewal-evil-cursors" "20200301.839" "`ewal'-colored evil cursor for Emacs and Spacemacs" '((emacs "25") (ewal "0.1")) :commit "732a2f4abb480f9f5a3249af822d8eb1e90324e3" :authors '(("Uros Perisic")) :maintainer '("Uros Perisic") :keywords '("faces") :url "https://gitlab.com/jjzmajic/ewal")
