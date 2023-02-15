b982ad9fa535e137015e90b195625b4ddcf0f5f2
aeon <aeonsec@keemail.me>
2023-02-14 22:10:39 -0300

    dotfiles

--- .config/suckless/dwm/config.def.h
+++ .config/suckless/dwm/config.def.h
@@ -240,6 +240,7 @@ static const Key keys[] = {
   { MODKEY,                       XK_u,          setlayout,         {.v = &layouts[11]} },
   { MODKEY,                       XK_space,      setlayout,         {0} },
   { MODKEY|ShiftMask,             XK_space,      togglefloating,    {0} },
+  { MODKEY,                       XK_0,          view,              {.ui = ~0 } },
   { MODKEY|ShiftMask,             XK_0,          tag,               {.ui = ~0 } },
   { MODKEY,                       XK_comma,      focusmon,          {.i = -1 } },
   { MODKEY,                       XK_period,     focusmon,          {.i = +1 } },
